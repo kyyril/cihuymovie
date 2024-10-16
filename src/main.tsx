1b326f84fecaba2a8fcd3984c9ed08f35069db24
kyyril <khairilrahman55@gmail.com>
2024-10-16 07:53:19 +0700

    create router

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,38 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App";
 import theme from "../theme";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <div>Home</div>,
+      },
+      {
+        path: "/movies",
+        element: <div>Movies</div>,
+      },
+      {
+        path: "/shows",
+        element: <div>TV Shows</div>,
+      },
+      {
+        path: "/search",
+        element: <div>Search</div>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <ChakraProvider theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
